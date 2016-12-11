12763be933a2dca2619053a5e989eeb2a8a939eb
Mieszko <mieszkobor@op.pl>
2016-12-11 16:52:11 +0100

    Add log system

--- log.php
+++ log.php
@@ -1,11 +1,15 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-if(!empty($_POST['data'])){
-	$data = $_POST['data'];
+header('Access-Control-Allow-Origin: *'); 
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
+
+	$data = file_get_contents('php://input');
 	$fname = uniqid(rand(), true) .'.log';
 	$file = fopen("logs/" .$fname, 'w');//creates new file
 	fwrite($file, $data);
 	fclose($file);
-}
 
 ?>
